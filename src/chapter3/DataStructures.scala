30f338fc191afb43ec2a6a2b5459ef9ee60fef6a
Christian Hausknecht <christian.hausknecht@gmx.de>
2017-07-15 18:39:34 +0200

    chapter3 and exercise 3.2

---
+++ src/chapter3/DataStructures.scala
@@ -0,0 +1,32 @@
+package chapter3
+
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(d, ds) => d * product(ds)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  /*
+  Excersice 3.2
+   */
+  def tail[A](as: List[A]): List[A] = as match {
+    case Nil => Nil // could also be an error!
+    case Cons(_, tail) => tail
+  }
+}
---
+++ src/listtest.sc
@@ -0,0 +1,12 @@
+import chapter3._
+
+
+val ex1: List[Double] = Nil
+val ex2: List[Int] = Cons(1, Nil)
+val ex3: List[String] = Cons("a", Cons("b", Nil))
+
+val ex4 = Cons(41, ex2)
+
+List.sum(ex4)
+
+List.tail(ex4)
