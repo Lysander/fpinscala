00f3723580ec76d5c499019dc45a98237b3027ab
Christian Hausknecht <christian.hausknecht@gmx.de>
2017-07-16 13:06:02 +0200

    until exercise 3.5

--- src/chapter3/DataStructures.scala
+++ src/chapter3/DataStructures.scala
@@ -15,7 +15,7 @@ object List {
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(d, ds) => d * product(ds)
+    case Cons(d, tail) => d * product(tail)
   }
 
   def apply[A](as: A*): List[A] =
@@ -23,10 +23,39 @@ object List {
     else Cons(as.head, apply(as.tail: _*))
 
   /*
-  Excersice 3.2
+  Exercise 3.2
    */
   def tail[A](as: List[A]): List[A] = as match {
     case Nil => Nil // could also be an error!
     case Cons(_, tail) => tail
   }
+
+  /*
+  Exercise 3.3
+   */
+  def setHead[A](head: A, as: List[A]): List[A] = as match {
+    case Nil => Cons(head, Nil)
+    case Cons(_, tail) => Cons(head, tail)
+  }
+
+  /*
+  Exercise 3.4
+   */
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+  if (n > 0) l match {
+    case Nil => l
+    case Cons(_, tail) => drop(tail, n - 1)
+  }
+  else l
+
+  /*
+  Exercise 3.5
+   */
+  @annotation.tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => l
+    case Cons(head, tail) => if (f(head)) dropWhile(tail, f) else l
+  }
+
 }
--- src/listtest.sc
+++ src/listtest.sc
@@ -10,3 +10,14 @@ val ex4 = Cons(41, ex2)
 List.sum(ex4)
 
 List.tail(ex4)
+
+List.setHead(69, ex4)
+
+val ex5 = List(1, 2, 3, 4, 5)
+
+List.drop(ex5, 3)
+List.drop(Nil, 1)
+List.drop(ex5, 6)
+
+List.dropWhile(ex5, (x: Int) => x < 3)
+
